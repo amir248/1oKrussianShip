1f37bca5bcb5dbb5c836d786a71dc32209eb7759
chikchicly <chikchicly@gmail.com>
2024-05-25 13:24:19 +0700

    there will be a menu here

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,24 @@
-document.querySelector('.menu')/addEventListener('click',()=>{
+document.querySelector('.menu').addEventListener('click',()=>{
   console.log('click');
+  let menuOpen=document.createElement('div');
+  menuOpen.style.cssText=`
+  display:flex;
+  position:fixed;
+  font-size:33px;
+  justify-content:center;
+  align-items:center;
+  text-shadow:1px 1px black;
+  top:0;
+  width:100%;
+  height:100%;
+  background:grey;
+  z-index:4;
+  `
+  menuOpen.setAttribute('id','menu');
+  menuOpen.innerHTML='there will be a menu here';
+
+  document.querySelector('nav').prepend(menuOpen);
+  document.getElementById('menu').addEventListener('click',()=>{
+    document.querySelector("#menu").remove();
+  });
 });
